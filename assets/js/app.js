781c852f92230293a55ed9feb43bdfa210e70cb0
lbferrand <lbferrand@gmail.com>
2022-04-23 16:44:20 -0400

    Add functionality to 'remove active class from link

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,14 +3,23 @@
 const menuToggleButton = document.querySelector(".menu-toggle-button");
 const menuElement = document.querySelector(".menu");
 
+// arrow function for menu (hamburger)
 const toggleMenu = () => {
   menuElement.classList.toggle("active");
+  menuToggleButton.classList.toggle("active");
 };
 
 menuToggleButton.addEventListener("click", toggleMenu);
 
 // !REMOVE ACTIVE CLASS FROM AND ICON ON LINK CLICK
+const removeActiveLinkClass = (e) => {
+  if (e.target.classList.contains("list-link")) {
+    menuElement.classList.remove("active");
+    menuToggleButton.classList.remove("active");
+  }
+};
 
+document.addEventListener("click", removeActiveLinkClass);
 // !TOGGLE THEME AND STORE SELECTION WITHIN LOCAL STORAGE
 
 // !SCROLL REVEAL
