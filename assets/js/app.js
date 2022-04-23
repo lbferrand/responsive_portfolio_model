e015d8206df21d75b32d859ead64680067ea62b9
lbferrand <lbferrand@gmail.com>
2022-04-23 16:27:09 -0400

    Add javascript for menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,14 @@
 //!TOGGLE NAVIGATION MENU AND ICONS
 
+const menuToggleButton = document.querySelector(".menu-toggle-button");
+const menuElement = document.querySelector(".menu");
+
+const toggleMenu = () => {
+  menuElement.classList.toggle("active");
+};
+
+menuToggleButton.addEventListener("click", toggleMenu);
+
 // !REMOVE ACTIVE CLASS FROM AND ICON ON LINK CLICK
 
 // !TOGGLE THEME AND STORE SELECTION WITHIN LOCAL STORAGE
