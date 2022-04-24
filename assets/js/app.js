0a2792a434a8d300deb86caa2aa6644032269280
lbferrand <lbferrand@gmail.com>
2022-04-24 18:45:15 -0400

    Add code to change them light to dark and vice-verse

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,6 +20,26 @@ const removeActiveLinkClass = (e) => {
 };
 
 document.addEventListener("click", removeActiveLinkClass);
+
 // !TOGGLE THEME AND STORE SELECTION WITHIN LOCAL STORAGE
+const themeToggleButton = document.querySelector(".theme-toggle-button");
+const bodyElement = document.body;
+const currentTheme = localStorage.getItem("darkTheme");
+
+if (currentTheme) {
+  bodyElement.classList.add("dark-theme");
+}
+
+const toggleTheme = () => {
+  bodyElement.classList.toggle("dark-theme");
+
+  if (bodyElement.classList.contains("dark-theme")) {
+    localStorage.setItem("darkTheme", "active");
+  } else {
+    localStorage.removeItem("darkTheme");
+  }
+};
+
+themeToggleButton.addEventListener("click", toggleTheme);
 
 // !SCROLL REVEAL
